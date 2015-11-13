437a93a1e888d6655224133db6d16dc959215c1c
jmachuca77 <jmachuca@prodigy.net.mx>
2015-11-13 10:26:35 -0600

    Add Intel Edison Ubilinux Modified Files
    This commit adds the Intel Edison Ublinux files for connecting to
    different networks, as well as for automatically starting MAVProxy on
    startup and the smart_camera.cnf script to enable the QX1 camera.

---
+++ Intel_Edison/Ubilinux/root/.local/bin/homenet.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Change Network to Home Network
+echo "Changing /etc/network/interfaces file"
+cp /etc/network/interfaces.home /etc/network/interfaces
+echo "Disable wlan0"
+ifdown wlan0
+echo "Re-enable wlan0"
+ifup wlan0
