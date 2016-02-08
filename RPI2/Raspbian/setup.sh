2de6a2a6a939684ef13beb115c4c5da34982a4c4
Randy Mackay <rmackay9@yahoo.com>
2016-02-08 10:29:04 +0900

    RPI2: add gstreamer and hostapd to setup scripts

--- RPI2/Raspbian/setup.sh
+++ RPI2/Raspbian/setup.sh
@@ -8,3 +8,9 @@ sudo easy_install python-pip
 sudo pip install dronekit dronekit-sitl # also installs pymavlink
 #sudo apt-get install screen python-wxgtk2.8 python-matplotlib python-opencv python-pip python-numpy python-dev libxml2-dev libxslt-dev
 sudo pip install mavproxy
+
+# live video related packages
+sudo apt-get install gstreamer1.0
+
+# access point packages
+sudo apt-get install hostapd isc-dhcp-server
