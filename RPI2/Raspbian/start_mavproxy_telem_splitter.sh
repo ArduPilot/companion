1aa5076270a328ff9441440534d1a9dfbb198f46
Randy Mackay <rmackay9@yahoo.com>
2016-02-24 19:48:21 +0900

    RPI2./Raspbian: start_mavproxy_telem_spliter uses bcast
    This allows us to output to just a single udp port

--- RPI2/Raspbian/start_mavproxy_telem_splitter.sh
+++ RPI2/Raspbian/start_mavproxy_telem_splitter.sh
@@ -1,2 +1,2 @@
 # this starts mavproxy so that the serial link to the companion computer (on /dev/ttyUSB0) is available to a companion computer and external GCSs via UDP
-mavproxy.py --master /dev/ttyUSB0 --baud 921600 --out udpin:localhost:9000 --out udpout:192.168.42.10:14550 --out udpout:192.168.42.11:14550 --out udpout:192.168.42.12:14550
+mavproxy.py --master /dev/ttyUSB0 --baud 921600 --out udpin:localhost:9000 --out udpbcast:192.168.42.255:14550
