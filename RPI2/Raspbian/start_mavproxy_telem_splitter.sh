4971e12d10be94cd04837388c8ae03f7d662b27d
Rustom Jehangir <rustyj110@gmail.com>
2016-03-22 11:58:56 -0700

    Change RPI for BR ArduSub project

--- RPI2/Raspbian/start_mavproxy_telem_splitter.sh
+++ RPI2/Raspbian/start_mavproxy_telem_splitter.sh
@@ -1,2 +1,15 @@
-# this starts mavproxy so that the serial link to the companion computer (on /dev/ttyUSB0) is available to a companion computer and external GCSs via UDP
-mavproxy.py --master /dev/ttyUSB0 --baud 921600 --out udpin:localhost:9000 --out udpbcast:192.168.42.255:14550
+# this starts mavproxy so that the serial link to the companion computer (on /dev/ttyACM0) 
+# is available to a companion computer and external GCSs via UDP. This broadcasts so that
+# multiple IP addresses can received the telemetry.
+
+# For PixHawk or other connected via USB on Raspberry Pi 2
+mavproxy.py --master /dev/ttyACM0 \
+            --baud 115200 \
+            --out udpin:localhost:9000 \
+            --out udpbcast:169.254.255.255:14550
+
+# For PixHawk or other connected via telem1 on Raspberry Pi 2
+# mavproxy.py --master /dev/ttyAMA0 \
+#             --baud 57600 \
+#             --out udpin:localhost:9000 \
+#             --out udpbcast:169.254.255.255:14550            
--- RPI2/Raspbian/start_video.sh
+++ RPI2/Raspbian/start_video.sh
@@ -1,2 +1,82 @@
-#raspivid -n -w 1280 -h 720 -b 1000000 -fps 15 -t 0 -o - | gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=10 pt=96 ! udpsink host=<remote_ip> port=9000
-raspivid -n -w 640 -h 480 -b 500000 -fps 15 -t 0 -o - | gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=10 pt=96 ! udpsink host=192.168.42.10 port=9000
+# Video Streaming Command
+# Please uncomment only one line depending on streaming preference
+
+# For all streaming options:
+# - Since we are hardwired through tether, bitrate is high to maximize quality
+# - Auto white balance is disabled
+# - Video stream is flushed to minimize latency
+# - Framerate is maximized
+# - Port 5000 is used for compatibility with QGroundControl
+
+# Low-latency, low resolution stream (100ms)
+
+# raspivid --nopreview \
+#          --width 640 \
+#          --height 480 \
+#          --bitrate 0 \
+#          --qp 10 \
+#          --framerate 30 \
+#          --awb off \
+#          --fl \
+#          --timeout 0 \
+#          --output - | \
+#          gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=10 pt=96 ! \
+#          udpsink host=192.168.2.1 port=5000
+
+# Reliable 720p stream with fairly low latency (200ms)
+
+# raspivid --nopreview \
+#          --width 1280 \
+#          --height 720 \
+#          --bitrate 0 \
+#          --qp 10 \
+#          --framerate 30 \
+#          --awb off \
+#          --fl \
+#          --timeout 0 \
+#          --output - | \
+#          gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=10 pt=96 ! \
+#          udpsink host=192.168.2.1 port=5000   
+
+# 1080p stream with some latency (300ms)
+
+# raspivid --nopreview \
+#          --width 1920 \
+#          --height 1080 \
+#          --bitrate 0 \
+#          --qp 10 \
+#          --framerate 30 \
+#          --awb off \
+#          --fl \
+#          --timeout 0 \
+#          --output - | \
+#          gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=10 pt=96 ! \
+#          udpsink host=192.168.2.1 port=5000                 
+
+# 1080p stream with some latency for wide angle RasPi camera (300ms)
+
+raspivid --nopreview \
+         --mode 2 \
+         --bitrate 0 \
+         --qp 10 \
+         --framerate 30 \
+         --awb off \
+         --fl \
+         --timeout 0 \
+         --output - | \
+         gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=10 pt=96 ! \
+         udpsink host=192.168.2.1 port=5000
+
+# 4K stream with latency (standard RasPiCam only)
+
+# raspivid --nopreview \
+#          --mode 2 \
+#          --bitrate 0 \
+#          --qp 10 \
+#          --framerate 15 \
+#          --awb off \
+#          --fl \
+#          --timeout 0 \
+#          --output - | \
+#          gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=10 pt=96 ! \
+#          udpsink host=192.168.2.1 port=5000     
