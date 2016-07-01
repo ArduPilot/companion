27095a08934d93f37195e5efc6ce901ba769a327
Rustom Jehangir <rustyj110@gmail.com>
2016-07-01 15:06:56 -0700

    Update RPi setup script.

--- RPI2/Raspbian/setup.sh
+++ RPI2/Raspbian/setup.sh
@@ -3,20 +3,26 @@
 # RPi2 setup script for use as companion computer. This script is simplified for use with 
 # the ArduSub code.
 
-# update RPI to latest versions
+# Update package lists and current packages
 sudo apt-get update
 sudo apt-get upgrade
+
+# Update Raspberry Pi
+sudo apt-get install -y rpi-update
 sudo rpi-update
 
 # install python and pip
-sudo apt-get install python-dev python-pip
+sudo apt-get install -y python-dev python-pip
 
 # install dronekit
 sudo pip install dronekit dronekit-sitl # also installs pymavlink
 sudo pip install mavproxy
 
+# install screen
+sudo apt-get install -y screen
+
 # live video related packages
-sudo apt-get install gstreamer1.0
+sudo apt-get install -y gstreamer1.0
 
 # Disable camera LED
 sudo sed '$a disable_camera_led=1' /boot/config.txt
