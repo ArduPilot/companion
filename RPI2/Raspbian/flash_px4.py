f2a4d8b7cb1c479e0c38b98d56a5ce2f134985c9
Rustom Jehangir <rustyj110@gmail.com>
2016-08-03 10:27:56 -0700

    Add utility to flash Pixhawk from Raspberry Pi.

---
+++ RPI2/Raspbian/flash_px4.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import os
+import urllib
+
+# Stop screen session with mavproxy
+print "Stopping mavproxy"
+os.system("sudo screen -X -S mavproxy quit")
+
+# Download most recent firmware
+print "Downloading latest ArduSub firmware..."
+firmwarefile = urllib.URLopener()
+firmwarefile.retrieve("http://firmware.ardusub.com/Sub/latest/PX4-vectored/ArduSub-v2.px4", "/tmp/ArduSub-v2.px4")
+
+# Download flashing script
+print "Downloading px4 flashing tool..."
+firmwarefile = urllib.URLopener()
+firmwarefile.retrieve("https://raw.githubusercontent.com/PX4/Firmware/master/Tools/px_uploader.py", "/tmp/px_uploader.py")
+
+# Flash Pixhawk
+print "Flashing Pixhawk..."
+os.system("python /tmp/px_uploader.py --port /dev/ttyACM0 /tmp/ArduSub-v2.px4")
+
+# Start screen session with mavproxy
+print "Restarting mavproxy"
+os.system("sudo screen -dm -S mavproxy /home/pi/companion/RPI2/Raspbian/start_mavproxy_telem_splitter.sh")
