d84499f16fc8af109f2ae75dea3580b426e48955
Randy Mackay <rmackay9@yahoo.com>
2016-12-06 14:38:25 +0900

    Nvidia_TX1: include wifi access point setup in setup scripts
    Also do not require user to press "y" before each package is installed

--- Nvidia_JTX1/Ubuntu/2_install_packages.sh
+++ Nvidia_JTX1/Ubuntu/2_install_packages.sh
@@ -7,20 +7,20 @@ sudo apt-get purge modemmanager
 sudo apt-get update
 
 # install python, numpy, pip
-sudo apt-get install python-dev python-numpy python3-numpy python-pip python-opencv
+sudo apt-get -y install python-dev python-numpy python3-numpy python-pip python-opencv
 
 # install dronekit
-sudo pip install dronekit dronekit-sitl # also installs pymavlink
+sudo pip -y install dronekit dronekit-sitl # also installs pymavlink
 
 # install mavproxy
-sudo pip install mavproxy
+sudo pip -y install mavproxy
 
 # live video related packages
-sudo apt-get install gstreamer1.0
+sudo apt-get -y install gstreamer1.0
 
 # install git
-sudo apt-get install git
+sudo apt-get -y install git
 
 # setup wifi access point.
 # NOTE: if something goes wrong with this setup, and you are unable to connect to the TX1, connect a keyboard and mouse and type "sudo dhclient eth0"
-sudo apt-get install hostapd dnsmasq haveged
+sudo apt-get -y install hostapd dnsmasq haveged
---
+++ Nvidia_JTX1/Ubuntu/3_wifi_access_point.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# this script sets up the wifi access point
+
+# NOTE: if something goes wrong with this setup, and you are unable to connect to the TX1, connect a keyboard and mouse and type "sudo dhclient eth0"
+sudo apt-get -y install hostapd dnsmasq haveged
+
+# disable network manager
+echo "manual" | sudo tee /etc/init/network-manager.override
+sudo mv /etc/init/network-manager.conf /etc/init/network-manager.conf.bak
+
+echo "options bcmdhd op_mode=2" | sudo tee -a /etc/modprobe.d/bcmdhd.conf
+
+# modify /etc/default/hostapd to use our config file
+sudo sed -i s/#DAEMON_CONF=\"\"/DAEMON_CONF='\"\/etc\/hostapd\/hostapd.conf\"/g' /etc/default/hostapd
+
+# copy our hostapd config file into place
+sudo cp hostapd.conf /etc/hostapd
+
+# add IP address range to /etc/dnsmasq.conf
+echo "interface=wlan0" | sudo tee -a /etc/dnsmasq.conf
+echo "dhcp-range=10.0.1.128,10.0.1.254,12h" | sudo tee -a /etc/dnsmasq.conf
+
+# disable NetworkManager so it does not interfere
+echo "manual" | sudo tee -a /etc/init/network-manager.override
+
+# create /etc/rc.local file to automatically start hostapd on each reboot
+sudo cp rc.local /etc
+# make sure /etc/rc.local is executable
+chmod a+x /etc/rc.local
