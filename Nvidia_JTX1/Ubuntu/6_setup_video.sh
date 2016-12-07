9e4a64a620f5db8f347e837d6bf7e1c1762e3d9f
Randy Mackay <rmackay9@yahoo.com>
2016-12-07 13:53:39 +0900

    Nvidia_TX1: add start video scripts

---
+++ Nvidia_JTX1/Ubuntu/6_setup_video.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# this script sets up the live video streaming
+
+# setup live video via http (complements of Krita from NII)
+# install and build http-launch
+sudo apt-get install git build-essential dpkg-dev flex bison autoconf autotools-dev automake liborc-dev autopoint libtool gtk-doc-tools libgstreamer1.0-dev
+
+mkdir ~/GitHub
+cd ~/GitHub
+git clone https://github.com/sdroege/http-launch
+cd http-launch
+export PKG_CONFIG_PATH=/home/ubuntu/GitHub/http-launch/out/lib/pkgconfig
+./autogen.sh
+./configure --prefix=/home/ubuntu/GitHub/http-launch/out
+make
+make install
+
+# copy startup scripts
+mkdir ~/start_video
+cp start_video.sh ~/start_video
+cp autostart_video.sh ~/start_video
+
+# add line below to bottom of /etc/rc.local to call $HOME/start_mavproxy/autostart_mavproxy.sh
+echo "sudo -H -u ubuntu /bin/bash -c '/home/ubuntu/start_video/autostart_video.sh'" | sudo tee -a /etc/rc.local
---
+++ Nvidia_JTX1/Ubuntu/start_video.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# start http video streaming
+$HOME/GitHub/http-launch/out/bin/http-launch 1234 v4l2src device=/dev/video0 ! "video/x-raw,width=640,height=480,framerate=15/1" ! jpegenc ! multipartmux streamable=true name=stream
