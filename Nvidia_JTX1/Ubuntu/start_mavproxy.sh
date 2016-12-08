75ea831cf542a8036fb8ac7b8fdd474b4856aa7f
Randy Mackay <rmackay9@yahoo.com>
2016-12-08 11:50:34 +0900

    Nvidia_TX1: Pixhawk connection moved to /dev/ttyTHS1
    This avoids startup problems when using the console

--- Nvidia_JTX1/Ubuntu/start_mavproxy.sh
+++ Nvidia_JTX1/Ubuntu/start_mavproxy.sh
@@ -3,4 +3,4 @@
 #    - main connection to flight controller using Serial 0 at 921k
 #    - udp connection on port 9000 for use by other processes
 #    - udp broadcast connection to allow multiple GCSs to connect to the flight controller via mavproxy
-mavproxy.py --master /dev/ttyS0 --baud 921600 --out udpin:localhost:9000 --out udpbcast:10.0.1.255:14550
+mavproxy.py --master /dev/ttyTHS1 --baud 921600 --out udpin:localhost:9000 --out udpbcast:10.0.1.255:14550
