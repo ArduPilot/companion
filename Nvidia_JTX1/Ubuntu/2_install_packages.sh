d8c908cb2bf6a479928f28dcbda1e597c8c8f96c
Randy Mackay <rmackay9@yahoo.com>
2016-12-09 09:54:33 +0900

    Nvidia_TX1: setup upgrades packages

--- Nvidia_JTX1/Ubuntu/2_install_packages.sh
+++ Nvidia_JTX1/Ubuntu/2_install_packages.sh
@@ -6,6 +6,10 @@ sudo apt-get purge -y modemmanager
 # update package info
 sudo apt-get update
 
+# upgrade packages
+sudo apt-get dist-upgrade -y
+sudo apt-get upgrade -y
+
 # install python, numpy, pip
 sudo apt-get -y install python-dev python-numpy python3-numpy python-pip python-opencv
 
