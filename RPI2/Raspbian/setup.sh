954f048725b86dc5573ca48ba108db69de0b5f51
Jacob Walser <jwalser90@gmail.com>
2016-12-20 18:57:30 -0500

    Update raspbian setup.sh

--- RPI2/Raspbian/setup.sh
+++ RPI2/Raspbian/setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# RPi2 setup script for use as companion computer. This script is simplified for use with 
+# RPi2 setup script for use as companion computer. This script is simplified for use with
 # the ArduSub code.
 
 # Update package lists and current packages
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get upgrade -y
 
 # Update Raspberry Pi
 sudo apt-get install -y rpi-update
@@ -24,6 +24,10 @@ sudo apt-get install -y screen
 # live video related packages
 sudo apt-get install -y gstreamer1.0
 
+# install git, and clone bluerobotics companion repository
+sudo apt-get install -y git
+git clone https://github.com/bluerobotics/companion.git /home/pi/companion
+
 # Disable camera LED
 sudo sed -i '$a disable_camera_led=1' /boot/config.txt
 
@@ -33,7 +37,10 @@ S2='screen -dm -S video /home/pi/companion/RPI2/Raspbian/start_video.sh'
 
 # this will produce desired result if this script has been run already,
 # and commands are already in place
-sudo sed -i -e "\%$S1%d" \ # delete S1 if it already exists
--e "\%$S2%d" \ # delete S2 if it already exists
--e "0,/^[^#]*exit 0/s%%$S1\n$S2\n&%" \ # insert S1 and S2 above the first uncommented exit 0 line in the file
+# delete S1 if it already exists
+# delete S2 if it already exists
+# insert S1 and S2 above the first uncommented exit 0 line in the file
+sudo sed -i -e "\%$S1%d" \
+-e "\%$S2%d" \
+-e "0,/^[^#]*exit 0/s%%$S1\n$S2\n&%" \
 /etc/rc.local
