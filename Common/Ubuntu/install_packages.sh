e2cb82f30fb3dfd01df81ccc78480060d8495c35
Peter Barker <pbarker@barker.dropbear.id.au>
2017-01-05 15:51:24 +1100

    Common/Ubuntu: scripts for installing common packages

---
+++ Common/Ubuntu/install_packages.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [ $(id -u) -ne 0 ]; then
+   echo >&2 "Must be run as root"
+   exit 1
+fi
+
+set -e
+set -x
+
+# remove modemmanager
+apt-get purge -y modemmanager
+
+# update package info
+apt-get update
+
+# upgrade packages
+apt-get dist-upgrade -y
+
+# install python, numpy, pip
+apt-get -y install python-dev python-numpy python3-numpy python-pip python-opencv
+
+# install dronekit
+pip install dronekit dronekit-sitl # also installs pymavlink
+
+# install mavproxy
+pip install mavproxy
+apt-get -y install screen
+
+# install git
+apt-get -y install git
+
+# remove unused packages
+apt-get autoremove -y
+
+# clear cache
+apt-get clean
