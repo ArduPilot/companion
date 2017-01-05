5b21d89a2c12256c282ac897d0de228b894c6fb1
Peter Barker <pbarker@barker.dropbear.id.au>
2017-01-05 15:50:49 +1100

    Common/Ubuntu: scripts for setting up WiFi AP

---
+++ Common/Ubuntu/3_wifi_access_point.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+if [ $(id -u) -ne 0 ]; then
+   echo >&2 "Must be run as root"
+   exit 1
+fi
+
+set -e
+set -x
+
+# this script sets up the wifi access point
+
+# NOTE: if something goes wrong with this setup, and you are unable to connect to the TX1, connect a keyboard and mouse and type "sudo dhclient eth0"
+apt-get -y install dnsmasq haveged
+
+# stop dnsmasq from running outside of where we want it to:
+sudo systemctl disable dnsmasq
+
+# Create Access Point
+APNAME="WiFiAP"
+SSID="ardupilot"
+KEY="enRouteArduPilot"
+
+# add IP address range to /etc/dnsmasq.conf
+dd of=/etc/dnsmasq.d/$APNAME.conf <<EOF
+interface=wlan0
+dhcp-range=10.0.1.129,10.0.1.138,12h
+EOF
+
+systemctl disable networking
+systemctl enable NetworkManager
+systemctl start NetworkManager
+systemctl stop networking
+sudo systemctl disable dnsmasq
+
+nmcli connection add type wifi ifname wlan0 con-name $APNAME ssid $SSID
+nmcli connection modify $APNAME connection.autoconnect yes
+nmcli connection modify $APNAME 802-11-wireless.mode ap
+nmcli connection modify $APNAME 802-11-wireless.band bg
+nmcli connection modify $APNAME ipv4.method shared
+nmcli connection modify $APNAME wifi-sec.key-mgmt wpa-psk
+nmcli connection modify $APNAME ipv4.addresses 10.0.1.128/24
+nmcli connection modify $APNAME wifi-sec.psk "$KEY"
+nmcli connection up $APNAME
