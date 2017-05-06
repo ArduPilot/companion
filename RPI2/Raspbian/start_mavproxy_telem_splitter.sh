4bd5555316345b83a7c55b7c4d906ff5d65bf04c
Jacob Walser <jwalser90@gmail.com>
2017-05-05 20:36:40 -0400

    sp

--- RPI2/Raspbian/start_mavproxy_telem_splitter.sh
+++ RPI2/Raspbian/start_mavproxy_telem_splitter.sh
@@ -1,11 +1,11 @@
 # this starts mavproxy so that the serial link to the companion computer (on /dev/ttyACM0) 
 # is available to a companion computer and external GCSs via UDP. This broadcasts so that
-# multiple IP addresses can received the telemetry.
+# multiple IP addresses can receive the telemetry.
 
 # For PixHawk or other connected via USB on Raspberry Pi
 cd /home/pi
 mavproxy.py --master=/dev/serial/by-id/usb-3D_Robotics_PX4_FMU_v2.x_0-if00,115200 \
-	    --source-system=200 \
+            --source-system=200 \
             --cmd="set heartbeat 0" \
             --out udpin:localhost:9000 \
             --out udpbcast:192.168.2.255:14550
