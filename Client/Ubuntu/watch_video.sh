156442c3437c65e0eb3b7a1b06b8595f8dc48a0d
Jacob Walser <jwalser90@gmail.com>
2017-05-16 19:50:09 -0400

    Remove chaff

--- Client/Ubuntu/watch_video.sh
+++
@@ -1,1 +0,0 @@
-gst-launch-1.0 -v udpsrc port=9000 caps='application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264' ! rtph264depay ! avdec_h264 ! videoconvert ! ximagesink
--- Intel_Edison/Ubilinux/root/.local/bin/homenet.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Change Network to Home Network
-echo "Changing /etc/network/interfaces file"
-cp /etc/network/interfaces.home /etc/network/interfaces
-echo "Disable wlan0"
-ifdown wlan0
-echo "Re-enable wlan0"
-ifup wlan0
