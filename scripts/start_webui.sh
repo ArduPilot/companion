817c7ecb31b456c61fac10a1adf02010c7607509
Jacob Walser <jwalser90@gmail.com>
2017-05-23 10:33:01 -0400

    Truncate logfile by lines instead of bytes

--- scripts/start_webui.sh
+++ scripts/start_webui.sh
@@ -3,7 +3,7 @@
 cd $HOME/companion/br-webui/
 
 # limit logfile size
-tail -c 2MB /home/pi/.webui.log > /tmp/.webui.log
+tail -n 10000 /home/pi/.webui.log > /tmp/.webui.log
 cp /tmp/.webui.log /home/pi/.webui.log
 rm -f /tmp/.webui.log
 
