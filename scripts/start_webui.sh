d40b8c6f1dc2154521bee415e352e6df75fcda0d
Jacob Walser <jwalser90@gmail.com>
2017-05-23 10:00:25 -0400

    Make logfile persistent, limit size to 2MB

--- scripts/start_webui.sh
+++ scripts/start_webui.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 cd $HOME/companion/br-webui/
-node index.js 2>&1 | tee -a /tmp/webui.log
+
+# limit logfile size
+tail -c 2MB /home/pi/.webui.log > /tmp/.webui.log
+cp /tmp/.webui.log /home/pi/.webui.log
+rm -f /tmp/.webui.log
+
+node index.js 2>&1 | tee -a /home/pi/.webui.log
