c8ab4df71ada1cd71ddb94ab597f079aecfbe62f
Peter Barker <pbarker@barker.dropbear.id.au>
2017-07-03 17:15:54 +1000

    Ubuntu: add pymavlink deps

--- Common/Ubuntu/install_packages.sh
+++ Common/Ubuntu/install_packages.sh
@@ -20,6 +20,10 @@ apt-get dist-upgrade -y
 # install python, numpy, pip
 apt-get -y install python-dev python-numpy python3-numpy python-pip python-opencv
 
+# pymavlink deps:
+apt-get install -y libxml2-dev libxslt1.1 libxslt1-dev
+time pip install future lxml
+
 # install dronekit
 pip install dronekit dronekit-sitl # also installs pymavlink
 
