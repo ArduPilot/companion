b5358b7ff9868a8cae2a2a0a4fa5877c9b87b420
Jacob Walser <jwalser90@gmail.com>
2017-07-13 22:18:01 -0400

    formatting

--- scripts/start_raspivid.sh
+++ scripts/start_raspivid.sh
@@ -1,34 +1,3 @@
 export LD_LIBRARY_PATH=/usr/local/lib/
 
-# Video Streaming Command
-# Please uncomment only one line depending on streaming preference
-
-# For all streaming options:
-# - Since we are hardwired through tether, bitrate is high to maximize quality
-# - Auto white balance is disabled
-# - Video stream is flushed to minimize latency
-# - Framerate is maximized
-# - Port 5600 is used for compatibility with QGroundControl
-
-# Low-latency, low resolution stream (100ms)
-
-# raspivid --nopreview \
-#          --width 640 \
-#          --height 480 \
-#          --bitrate 10000000
-#          -fps 30 \
-#          --awb off \
-#          -roi 0.02,0.0,0.81,0.85 \
-#          -fl \
-#          --timeout 0 \
-#          --output - | \
-#          gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=10 pt=96 ! \
-#          udpsink host=192.168.2.1 port=5600
-
-# HD Stream with some latency for RasPi camera (250ms on RPi3)
-# Use mode 2 for 1080p @ 15 fps and mode 5 for 922p @ 40 fps
-
-
-#./start_raspivid.sh '--nopreview --mode 5 --bitrate 15000000 --intra 1 --awb auto --brightness 55 --saturation 10 --sharpness 50 --contrast 15  -fl --timeout 0' 'h264parse ! rtph264pay config-interval=10 pt=96 ! udpsink host=192.168.2.1 port=5600'
-         
 raspivid $1 --output - | gst-launch-1.0 -v fdsrc $2
