51d741f230e0cabe413db707ceae162ddd244a43
dheideman <dheideman@socal.rr.com>
2017-08-18 16:45:27 -0700

    Add command line options to gps-replay

--- tools/gps-replay.py
+++ tools/gps-replay.py
@@ -2,21 +2,29 @@
 
 import time
 import socket
+import argparse
 
-file = open('/home/pi/companion/tools/raw-nmea-log', 'r')
+parser = argparse.ArgumentParser(description="GPS Replay Tool for Testing and Debugging")
+parser.add_argument('--frequency', action="store", type=float, default=10, help="gps message update frequency")
+parser.add_argument('--file', action="store", type=str, default='/home/pi/companion/tools/raw-nmea-log', help="gps log file from which to read")
+parser.add_argument('--port', action="store", type=int, default=27000, help="udp destination port")
+parser.add_argument('--ip', action="store", type=str, default="0.0.0.0", help="udp destination ip address")
+args = parser.parse_args()
+
+file = open(args.file, 'r')
 content = file.readlines()
 file.close()
 
-ip="0.0.0.0"
-portnum = 27000
+delay = 1.0/args.frequency
+
 sockit = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sockit.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sockit.setblocking(False)
 
 while True:
     for line in content:
-        time.sleep(0.1)
+        time.sleep(delay)
         try:
-            sockit.sendto(line, (ip, portnum))
+            sockit.sendto(line, (args.ip, args.port))
         except Exception as e:
             print e
