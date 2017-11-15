28b9433d334941199560bf77e2fb9bd1df706527
Jacob Walser <jwalser90@gmail.com>
2017-11-14 21:30:10 -0500

    Set up gstreamer audio stream to udpsink on 192.168.2.1:5601
    And stop the 'easter egg' stream to the webui

--- scripts/start_audio.sh
+++ scripts/start_audio.sh
@@ -1,9 +1,9 @@
 export LD_LIBRARY_PATH=/usr/local/lib/
 cd /home/pi/
-#gst-launch-1.0 -v -e alsasrc device=hw:1,0 ! audioconvert ! rtpL16pay ! udpsink host=192.168.2.1 port=5700
+gst-launch-1.0 -v -e alsasrc device=hw:1,0 ! audioconvert ! rtpL16pay ! udpsink host=192.168.2.1 port=5601
 
 
 #gst-launch-1.0 -v -e alsasrc device=hw:1,0 ! audioconvert ! tcpserversink host=192.168.2.2 port=5700
 
 # This one fo-sho works
-gst-launch-1.0 -v -e alsasrc device=hw:1,0 ! audioconvert ! lamemp3enc ! tcpserversink host=192.168.2.2 port=5700
+#gst-launch-1.0 -v -e alsasrc device=hw:1,0 ! audioconvert ! lamemp3enc ! tcpserversink host=192.168.2.2 port=5700
