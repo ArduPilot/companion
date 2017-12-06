353ccaa4b27614dff39fff5c6cd26850049cd555
Peter Barker <pbarker@barker.dropbear.id.au>
2017-12-07 10:57:59 +1100

    Nvidia_JTX1: remove directory-specific line on download_image.sh

--- Nvidia_JTX1/Ubuntu/download_image.sh
+++ Nvidia_JTX1/Ubuntu/download_image.sh
@@ -1,3 +1,2 @@
-cd ~/JetPack-2.3.1/64_TX1/Linux_for_Tegra_64_tx1/bootloader
 read -n 1 -p "Connect TX with USB cable and put into bootloader mode (Hold Force-Recovery, Press Reset, release Force-Recovery).  Then press any key to begin downloading to $1."
 sudo ./tegraflash.py --bl cboot.bin --applet nvtboot_recovery.bin --chip 0x21 --cmd "read APP $1"
