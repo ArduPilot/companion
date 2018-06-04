4ab24d02b95674f0b015474d9cd54fb87b6b38ae
mariyachanniwala <msc.id52@gmail.com>
2018-06-04 18:24:28 +0000

    Download webui logs functionality added!

--- br-webui/index.js
+++ br-webui/index.js
@@ -9,6 +9,7 @@ var env = process.env
 logger.log('ENVIRONMENT', process.env)
 logger.log('COMPANION_DIR', process.env.COMPANION_DIR)
 app.use(express.static('public'));
+app.use('/webui.log', express.static('/home/pi/.webui.log'));
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/font-awesome', express.static(__dirname + '/node_modules/font-awesome')); // redirect JS jQuery
