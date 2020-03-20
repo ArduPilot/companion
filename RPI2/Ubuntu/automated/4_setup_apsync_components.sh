dc55f9d8c7b8b27ebed0430c210df8d5868b1426
Randy Mackay <rmackay9@yahoo.com>
2020-03-20 15:33:40 +0900

    RPI2/Ubuntu: 4_setup_apsync_components loses swap file creation
    also remove commented out lines

--- RPI2/Ubuntu/automated/4_setup_apsync_components.sh
+++ RPI2/Ubuntu/automated/4_setup_apsync_components.sh
@@ -11,13 +11,9 @@ set -x
 . config.env
 
 pushd /home/$NORMAL_USER/GitHub/companion/RPI2/Ubuntu
-time ./apstreamline.sh # 1m  This is optional
-#time ./setup_mavlink-router # ~2m Remember to change the mavlink_router.conf file to the right serial port
 time ./setup_master_mavlink-router.sh
 
 time ./7_dflogger.sh # ~210s
-#time ./5_setup_mavproxy.sh # instant
-#time ./setup-video-streaming # 11s  This is optional
 
 time apt-get install -y libxml2-dev libxslt1.1 libxslt1-dev
 time pip install future lxml # 4m
@@ -40,18 +36,8 @@ EOF
 
 time ./install_apweb # 2m
 
-tput setaf 3
-echo "Creating swap file for libRealsense build"
-tput sgr0
-
-time ./create_swap_file.sh
-
 tput setaf 2
 echo "Finished installing APSync Components"
-echo "Rebooting in 5 sec to enable swap file"
 
 tput sgr0
 popd
-
-sleep 5
-reboot # ensure swap file was created
