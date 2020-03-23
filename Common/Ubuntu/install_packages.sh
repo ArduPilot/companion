c53037c1387f6e2a642dc34bd50f3be08b4d356e
Randy Mackay <rmackay9@yahoo.com>
2020-03-23 15:09:27 +0900

    Common/Ubuntu: install pip3 and future

--- Common/Ubuntu/install_packages.sh
+++ Common/Ubuntu/install_packages.sh
@@ -18,11 +18,12 @@ apt-get update
 apt-get dist-upgrade -y
 
 # install python, numpy, pip
-apt-get -y install python-dev python-numpy python3-numpy python-pip python-opencv
+apt-get -y install python-dev python-numpy python3-numpy python-pip python-opencv python3-pip
 
 # pymavlink deps:
 apt-get install -y libxml2-dev libxslt1.1 libxslt1-dev libz-dev python-lxml
 time pip install future
+time pip3 install future
 
 # install dronekit
 pip install dronekit dronekit-sitl # also installs pymavlink
