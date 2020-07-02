799f8ed5a851afaf7686b592568636a25e109bc4
Randy Mackay <rmackay9@yahoo.com>
2020-07-02 14:53:41 +0900

    RPI2/Ubuntu: copy start/stop_wifi.sh into new start_wifi directory
    also enable access point on 5G by default

--- RPI2/Ubuntu/install_wifi_access_point.sh
+++ RPI2/Ubuntu/install_wifi_access_point.sh
@@ -20,8 +20,18 @@ sleep 5
 wifi-ap.config set wifi.ssid=ArduPilot
 wifi-ap.config set wifi.security-passphrase=ardupilot
 wifi-ap.config set wifi.address=10.0.1.128
-wifi-ap.config set wifi.channel=48
-wifi-ap.config set wifi.operation-mode=a
+wifi-ap.config set wifi.operation-mode=a    # 5Ghz by default
+wifi-ap.config set wifi.channel=48          # set to a clear channel in your area
 wifi-ap.config set dhcp.range-start=10.0.1.129
 wifi-ap.config set dhcp.range-stop=10.0.1.138
+wifi-ap.config set disabled=false
 
+# copy wifi scripts into place
+pushd /home/$NORMAL_USER/GitHub/companion/Common/Ubuntu/wifi_ap
+rm -rf /home/$NORMAL_USER/start_wifi
+mkdir /home/$NORMAL_USER/start_wifi
+cp start_wifi.sh /home/$NORMAL_USER/start_wifi
+cp start_wifi_2.4Ghz.sh /home/$NORMAL_USER/start_wifi
+cp start_wifi_5Ghz.sh /home/$NORMAL_USER/start_wifi
+cp stop_wifi.sh /home/$NORMAL_USER/start_wifi
+popd
