7768ffcda78cf609dd471b2f9b853ef5795ee37c
Randy Mackay <rmackay9@yahoo.com>
2020-09-01 14:51:00 +0900

    Up_Squared/Ubuntu: separate realsense and vision_to_mavros installation steps

--- Up_Squared/Ubuntu/7_setup_realsense.sh
+++ Up_Squared/Ubuntu/7_setup_realsense.sh
@@ -12,13 +12,8 @@ set -x
 
 pushd /home/$NORMAL_USER/GitHub/companion/Common/Ubuntu/librealsense
 time ./install_librealsense.sh
-time ./install_vision_to_mavros.sh
 popd
 
 tput setaf 2
-if [ $SETUP_DEPTH_CAMERA -eq 1 ]; then
-   echo 'Success! Finished part 7: installing Intel Realsense Drivers, Pose and Depth Scripts'
-else
-   echo 'Success! Finished part 7: installing Intel Realsense Drivers and Pose Script'
-fi
+echo 'Success! Finished part 7: installing Intel Realsense Drivers'
 tput sgr0
