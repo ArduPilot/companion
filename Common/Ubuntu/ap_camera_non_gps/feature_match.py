9f6f64b7ceab9b10429000e497c2d1a647ca214d
Sanket Sharma <sharma.sanket272@gmail.com>
2023-07-11 01:34:38 +0530

    Corrected and updated zoom scale
    Made code modular and corrected offset and zoom scaling

--- Common/Ubuntu/ap_camera_non_gps/feature_match.py
+++ Common/Ubuntu/ap_camera_non_gps/feature_match.py
@@ -1,21 +1,27 @@
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
-import subprocess
 import pyexiv2, json
 import math
 
 x_offset = 0
 y_offset = 0
-
-MIN_MATCH_COUNT = 30
-
-LOCATION_SCALING_FACTOR_INV = 89.83204953368922
-
 inCMX = 0
 inCMY = 0
 
+MIN_MATCH_COUNT = 30
+LOCATION_SCALING_FACTOR_INV = 89.83204953368922
+
+#user input ______________
 focal = 476.7030836014194
+latBase = -353632621
+lonbase = 1491652371
+
+baseImg = "test1.jpeg"
+testImg = "test2.jpeg"
+
+#__________________________
+
 
 
 
@@ -39,35 +45,48 @@ def modifyExif(image,data=None):
         f.seek(0)
         with pyexiv2.ImageData(f.read()) as img:
             result = img.read_exif()
-            #print(result)
     f.close()
+
+def calc_dist(x1, y1, x2, y2):
+    distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    return distance
 
 
 def compParam(img1,img2):
     global inCMY, inCMX
     global x_offset, y_offset
+
+    print('comparing ' + img1 + ' vs ' + img2 + "\n")
+
     img1 = cv.imread(img1,0)
     img2 = cv.imread(img2,0)
     #img2 = cv.resize(img2, (1778,866))
-    sift = cv.SIFT_create(nfeatures = 10000)
 
+    # extract key features from each image
+    sift = cv.SIFT_create(nfeatures = 10000)
     kp1, des1 = sift.detectAndCompute(img1,None)
     kp2, des2 = sift.detectAndCompute(img2,None)
+
+    # find matching features from each image
     FLANN_INDEX_KDTREE = 1
     index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
     search_params = dict(checks = 50)
     flann = cv.FlannBasedMatcher(index_params, search_params)
     matches = flann.knnMatch(des1,des2,k=2)
 
+    # create array of best matches
     good = []
     for m,n in matches:
         if m.distance < 0.7*n.distance:
             good.append(m)
 
 
+    # ensure we have at at least 30 matching points
     if len(good)>MIN_MATCH_COUNT:
-        src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
-        dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+
+        # display image showing matching points
+        src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1,1,2)
+        dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1,1,2)
         M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC,5.0)
         matchesMask = mask.ravel().tolist()
         h,w = img1.shape
@@ -75,49 +94,42 @@ def compParam(img1,img2):
         dst = cv.perspectiveTransform(pts,M)
         img2 = cv.polylines(img2,[np.int32(dst)],True,255,3, cv.LINE_AA)
 
-
-
-        #Orientation
-
+        # calculate rotation difference
         diff = np.abs(dst - pts)
         orientation_diff = np.arctan2(np.abs(dst[1][0][1] - dst[0][0][1]), np.abs(dst[1][0][0] - dst[0][0][0]))
         print("Orientation difference: {:.2f} degrees".format(90 - np.degrees(orientation_diff)))
 
-
-        #x, y offset
-
+        # calculate x, y offset
         h1, w1 = img1.shape[:2]
         h, w = img2.shape
 
-        sr_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
-        ds_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
-        Mr, maskr = cv.findHomography(ds_pts, sr_pts, cv.RANSAC, 5.0)
+        Mr, maskr = cv.findHomography(dst_pts, src_pts, cv.RANSAC, 5.0)
 
         corners = np.array([[0, 0], [0, h-1], [w-1, h-1], [w-1, 0]], dtype=np.float32).reshape(-1, 1, 2)
         transformed_corners = cv.perspectiveTransform(corners, Mr)
 
         x, y = np.mean(transformed_corners, axis=0).astype(int)[0]
 
-        print("Offset x, y: ",x-w1/2, y-h1/2)
+        print("Offset x, y(in cms): ",x-w1/2, y-h1/2)
 
         inCMX = (x-w1/2) * (10) / focal
         inCMY = (y-h1/2) * (10) / focal
 
 
-        #Zoom percentage -- Not complete
+        # calculate zoom percentage
+        binToBool= [True if n == 1 else False for n in matchesMask]
+        res = list(filter(lambda i: binToBool[i], range(len(binToBool))))
+        res_listQ = [src_pts[i].tolist() for i in res]
+        res_listT = [dst_pts[i].tolist() for i in res]
 
-        width = abs(transformed_corners[2][0][0]) - abs(transformed_corners[0][0][0])
-        height = abs(transformed_corners[2][0][1]) - abs(transformed_corners[0][0][1])
+        mainDist = calc_dist(res_listQ[0][0][0],res_listQ[0][0][1],res_listQ[-1][0][0],res_listQ[-1][0][1])
+        desDist = calc_dist(res_listT[0][0][0],res_listT[0][0][1],res_listT[-1][0][0],res_listT[-1][0][1])
 
-        im2 = width * height
-        im1 = img1.shape[0] * img1.shape[1]
-
-        #print(transformed_corners)
-
-        print(f"Zoom percent: {im2/im1*100} %")
+        scaleRatio = mainDist/desDist
+        print("Scaling Ratio:",scaleRatio) # multiplied by base altitude generates current altitude
 
     else:
-        print( "Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT) )
+        print("Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT) )
         matchesMask = None
 
 
@@ -127,17 +139,18 @@ def compParam(img1,img2):
                        flags = 2)
 
 
-    binToBool= [True if n == 1 else False for n in matchesMask]
-    res = list(filter(lambda i: binToBool[i], range(len(binToBool))))
-    res_listQ = [src_pts[i].tolist() for i in res]
-    res_listT = [dst_pts[i].tolist() for i in res]
+
 
 
     cv.namedWindow("img3", cv.WINDOW_NORMAL)
     img3 = cv.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
-
     cv.imshow('img3', img3)
+    
+    # calculate new lat and lng based upon pixel offset
+    # To-Do: get lat,lon,alt from image1 or user
+    offset_latlng(latBase,lonbase,-inCMX,inCMY)
 
+    print("Waiting for key press")
     cv.waitKey(0)
 
 
@@ -148,7 +161,7 @@ def offset_latlng(lat,lng,ofs_north,ofs_east):
     lat += dlat
     lat = limit_lattitude(lat)
     lng = wrap_longitude(dlng+lng)
-    print("New Lat & Lng: ",lat,lng)
+    print("Updated Lat:",lat,"\n","Updated Lon:",lng)
 
 
 def longitude_scale(lat):
@@ -170,11 +183,13 @@ def wrap_longitude(lon):
     elif lon < -1800000000:
         lon = int(lon+3600000000)
     return int(lon)
-    
-    
+
 if __name__ == '__main__':
 
-    compParam("test1.png",'test2.png') #To find the features and get orientation, offset and zoom
+    compParam(baseImg, testImg)
 
-    offset_latlng(-353632621,1491652378,-inCMX,inCMY) #Extract new lat and lng based upon pixel offset
+    #offset_latlng(latBase,lonbase,-inCMX,inCMY)
 
+    # metadata = {"Latitude": "192.11.123", "Longitude": "172.11.12", "AMSL": "10", "Terr_Alt": "5"}
+    # modifyExif('test1.jpeg', metadata)
+    # readExif('test1.jpeg')
